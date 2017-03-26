9687f6b1735bc6201203fd4b555c0e07524cb653
rayduck <ruida@dingansi.ch>
2017-03-26 23:14:02 +0800

    adds secret codes and some songs

---
+++ index.js
@@ -0,0 +1,51 @@
+function playSound (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if (!audio) return
+  audio.currentTime = 0 // rewind to start so we can spam keys
+  audio.play()
+  key.classList.add('playing')
+};
+
+function removeTransition (e) {
+  if (e.propertyName !== 'transform') return
+  this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+window.addEventListener('keydown', playSound)
+
+// secretCode
+let pressed = []
+const secretEffects = new Map()
+  .set('trumpmeup', () => playMusic('trump'))
+  .set('shutthefuckup', () => stopMusic())
+  .set('aesthetic', () => playMusic('vaporwave'))
+  .set('bitesthedust', () => playMusic('kira'))
+  .set('normiesgetout', () => playMusic('reee'))
+const longestCodeLength = [...secretEffects.keys()].reduce((a, b) => a.length > b.length ? a : b).length
+
+function secretController (inputs) {
+  for (let [key, value] of secretEffects) {
+    if (inputs.includes(key)) {
+      value()
+      pressed = []
+    }
+  }
+}
+
+window.addEventListener('keyup', (e) => {
+  pressed.push(e.key)
+  pressed.splice(-longestCodeLength - 1, pressed.length - longestCodeLength)
+  secretController(pressed.join(''))
+})
+
+function playMusic (id) {
+  document.querySelector(`#${id}`).play()
+}
+
+function stopMusic () {
+  Array.from(document.querySelectorAll('audio')).forEach(audio => { audio.pause() })
+}
